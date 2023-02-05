3bc4695690a54c382cd5c14fc6224d50b737d50a
akashjangra89 <devloper64@gmail.com>
2023-02-05 16:19:45 +0530

    added set timeout to copied text

--- random_color_palette/script.js
+++ random_color_palette/script.js
@@ -10,7 +10,7 @@ const randomPalette = () =>{
         let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
         randomHex = `#${randomHex.padStart(6,'0')}`
 
-        
+
         html += `<ul class='list'>
         <li class="color">
             <div class="box" style ='background:${randomHex}'></div>
@@ -24,6 +24,19 @@ const randomPalette = () =>{
 randomPalette()
 
 btn.addEventListener('click',  randomPalette)
+container.addEventListener('click', function(event) {
+    if (event.target.className === 'hexValue') {
+      const text = event.target.textContent
+      navigator.clipboard.writeText(text).then(function() {
+        event.target.innerHTML = `Copied`
+        setTimeout(function(){
+            event.target.innerHTML = text
+        },1000)
+      }, function(err) {
+        console.error('Could not copy text: ', err)
+      })
+    }
+  })
 
 
 
